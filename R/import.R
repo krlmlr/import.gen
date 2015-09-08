652fa27c4e6244c7a0217eb512a91b435e21938a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-09-08 15:18:11 +0200

    add masked symbols as comments

--- R/import.R
+++ R/import.R
@@ -31,21 +31,26 @@ globalVariables(c("."))
 #' from("rpart", .output = "cat")
 #'
 #' @importFrom magrittr %>%
+#' @importFrom dplyr filter
 #' @export
 from <- function(..., .pkgs = NULL, .output = c("clipboard", "cat", "return"),
                  .comment = TRUE) {
   .output <- match.arg(.output)
   .dots <- list(...)
-  ret <- lapply(c(unname(.dots), .pkgs), from_one) %>%
-    unlist
+  symbols <- find_symbols(c(unname(.dots), .pkgs))
+  ret <- from_symbols(filter(symbols, keep))
   if (.comment) {
     my_call <- c(list("from"), .dots, if (!is.null(.pkgs)) list(.pkgs = .pkgs)) %>%
       do.call(call, .) %>%
       call("::", as.name("import.gen"), .)
 
+    ignore <- paste("#", from_symbols(filter(symbols, !keep)))
+
     ret <- c(
       "# The imports below were generated using the following call:",
       paste0("# ", format(my_call)),
+      if (length(ignore) > 0L) "# The following symbols could not be imported:",
+      ignore,
       ret
     )
   }
@@ -58,13 +63,35 @@ from <- function(..., .pkgs = NULL, .output = c("clipboard", "cat", "return"),
 }
 
 #' @importFrom magrittr %>%
-from_one <- function(pkg) {
+#' @importFrom dplyr tbl_df group_by ungroup do mutate data_frame
+#' @importFrom kimisc list_to_df
+find_symbols <- function(pkgs) {
+  exports <-
+    pkgs %>%
+    setNames(nm = .) %>%
+    lapply(getNamespaceExports)
+
+  exports_df <-
+    exports %>%
+    list_to_df %>%
+    group_by(name) %>%
+    do(data_frame(symbol = unlist(.$value))) %>%
+    ungroup %>%
+    mutate(keep = !duplicated(symbol, fromLast = TRUE))
+
+  exports_df
+}
+
+#' @importFrom magrittr %>%
+from_symbols <- function(pkg) {
   pkg %>%
-    {c(., getNamespaceExports(.) %>% sort)} %>%
-    as.list %>%
-    c(call("::", as.name("import"), as.name("from")), .) %>%
-    as.call %>%
-    format
+    group_by(name) %>%
+    do(data_frame(format = {
+      c(call("::", as.name("import"), as.name("from")), .$name[[1L]], .$symbol) %>%
+        as.call %>%
+        format
+    })) %>%
+    extract2("format")
 }
 
 message_after <- function(code, msg) {
